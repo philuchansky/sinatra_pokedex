09269d01d7fee0f8bb58875f3fdd6da77a2d96fc
Philippe Luchansky <philuchansky@gmail.com>
2015-10-11 18:41:39 -0700

    basic home view and json listing of pokemon

--- app.rb
+++ app.rb
@@ -7,8 +7,17 @@ require 'rest-client'
 
 get '/' do
 	@title = "Pokedex!"
+
+	api_result = RestClient.get "http://pokeapi.co/api/v1/pokedex"
+	@pokemon = JSON.parse(api_result)["objects"][0]["pokemon"]
+	erb :home
 end
 
 get '/json' do
 	@title = "Pokedex! JSON"
+
+	api_result = RestClient.get "http://pokeapi.co/api/v1/pokedex"
+	@pokemon = JSON.parse(api_result)["objects"][0]["pokemon"]
+	@pokemon.to_json
+
 end
